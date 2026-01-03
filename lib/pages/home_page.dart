e66b6fbb61a356150ee3f7b837b53d837e969e0e
jaeqwrty <jaesmalla1@gmail.com>
2026-01-04 00:04:06 +0800

    TESTING PHASE v2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unused_result
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
